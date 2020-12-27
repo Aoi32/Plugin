b2cb073effad7dae6876046140411a0dc69a228c
Aoba <54072033+Aoi32@users.noreply.github.com>
2020-12-27 19:54:59 +0900

    Update cheat.cpp

--- src/CTRPF/cheat.cpp
+++ src/CTRPF/cheat.cpp
@@ -4,6 +4,7 @@ namespace CTRPluginFramework
 {
 void	test(MenuEntry *entry)
 {
+	/* 0xXXXXXXXX <- Address Y.Y <- Data */
 	if(Controller::IsKeysDown(B+DPadRight)) ADDTOFLOAT(0xXXXXXXXX, Y.Y);	//B+十字右
 	if(Controller::IsKeysDown(B+DPadLeft)) ADDTOFLOAT(0xXXXXXXXX, -Y.Y);	//B+十字左
 	if(Controller::IsKeysDown(B+DPadUp)) ADDTOFLOAT(0xXXXXXXXX, Y.Y);	//B+十字上
